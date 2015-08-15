b42331cc6af608ed3a40c56cec0834bea878456a
David Hollinger III <david.hollinger@moduletux.com>
2015-08-14 20:34:00 -0500

    added firewalld_service resource type. Basic functionality

---
+++ lib/puppet/provider/firewalld_service/firewall_cmd.rb
@@ -0,0 +1,21 @@
+Puppet::Type.type(:firewalld_service).provide('firewall_cmd') do
+  
+  commands :firewall_cmd => '/usr/bin/firewalld'
+  
+  def exists?
+    firewall_cmd('--list-services').split(' ').include?(resource[:service])
+  end
+  
+  def create
+    rule=resource[:service]
+    firewall_cmd('--permanent','--add-service', rule)
+    firewall_cmd('--reload')
+  end
+  
+  def destroy
+    rule=resource[:service]
+    firewall_cmd('--permanent','--add-service', rule)
+    firewall_cmd('--reload')
+  end
+  
+end

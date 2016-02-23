38772113dceb69c94d87348ed7dec28db69cf47e
David Hollinger III <david.hollinger@moduletux.com>
2016-02-22 22:16:01 -0600

    Refactor firewalld_port provider

--- lib/puppet/provider/firewalld_port/firewall_cmd.rb
+++ lib/puppet/provider/firewalld_port/firewall_cmd.rb
@@ -1,24 +1,29 @@
 Puppet::Type.type(:firewalld_port).provide('firewall_cmd') do
   commands :firewall_cmd => '/usr/bin/firewall-cmd'
 
+  @rule="#{resource[:port]}/#{resource[:protocol]}"
+
   def firewalld_reload
     cmd = %x{`/usr/bin/firewall-cmd --reload`}
     raise Puppet::Error, "Failed to reload firewall changes for #{@resource[:service]} : #{cmd}" unless $?.success?
   end
 
   def exists?
-    firewall_cmd('--list-ports').split(' ').include?("#{resource[:port]}/#{resource[:protocol]}")
+    result = firewall_cmd('--query-port', @rule)
+    if result == 'yes'
+      true
+    else
+      false
+    end
   end
 
   def create
-    rule="#{resource[:port]}/#{resource[:protocol]}"
-    firewall_cmd('--permanent','--add-port', rule)
+    firewall_cmd('--permanent','--add-port', @rule)
     firewalld_reload
   end
 
   def destroy
-    rule="#{resource[:port]}/#{resource[:protocol]}"
-    firewall_cmd('--permanent','--remove-port', rule)
+    firewall_cmd('--permanent','--remove-port', @rule)
     firewalld_reload
   end
 end

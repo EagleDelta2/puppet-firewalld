b14027e3919f674ff97c64a6a177c8c847c44dbb
David Hollinger III <david.hollinger@moduletux.com>
2016-02-22 21:41:07 -0600

    Add firewalld_reload method and calls to it

--- lib/puppet/provider/firewalld_port/firewall_cmd.rb
+++ lib/puppet/provider/firewalld_port/firewall_cmd.rb
@@ -1,6 +1,11 @@
 Puppet::Type.type(:firewalld_port).provide('firewall_cmd') do
   commands :firewall_cmd => '/usr/bin/firewall-cmd'
 
+  def firewalld_reload
+    cmd = %x{`/usr/bin/firewall-cmd --reload`}
+    raise Puppet::Error, "Failed to reload firewall changes for #{@resource[:service]} : #{cmd}" unless $?.success?
+  end
+
   def exists?
     firewall_cmd('--list-ports').split(' ').include?("#{resource[:port]}/#{resource[:protocol]}")
   end
@@ -8,12 +13,12 @@ Puppet::Type.type(:firewalld_port).provide('firewall_cmd') do
   def create
     rule="#{resource[:port]}/#{resource[:protocol]}"
     firewall_cmd('--permanent','--add-port', rule)
-    firewall_cmd('--reload')
+    firewalld_reload
   end
 
   def destroy
     rule="#{resource[:port]}/#{resource[:protocol]}"
     firewall_cmd('--permanent','--remove-port', rule)
-    firewall_cmd('--reload')
+    firewalld_reload
   end
 end
